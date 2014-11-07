373d8a80f2d5c451def4c0d61361fe330c9f97aa
JoyceMiryamH <jhabb071@uottawa.ca>
2014-11-07 12:52:04 -0500

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,36 @@
+/*
+ * SavingsAccount
+ *
+ * Created on May 20th, 2014
+ * Last updated on June 4th, 2014
+ *
+ * Name:        Joyce Habbouche
+ * Course:      1121 X
+ * Assignment:  1
+ */
+public class SavingsAccount extends Account {
+  
+  /**
+     * Class constructor; 
+     *
+     */
+  
+  public SavingsAccount(Customer customer, String accountNumber) {
+    super(customer, accountNumber);
+  } 
+  
+  // deposit method
+  public void deposit(double amount) {
+  this.setBalance(this.getBalance() + amount);
+  }
+
+  // withdraw method
+  public void withdrawl(double amount) {
+    this.setBalance(this.getBalance() - amount);
+  }
+
+  // method to add interest
+  public void addInterest(double interest) {
+    this.setBalance(this.getBalance()*(1+interest));
+  }
+}
